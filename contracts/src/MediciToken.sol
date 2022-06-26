77b1415a42e211b58cc087921f8428044e6eb925
NME.eth <maximus.holm@gmail.com>
2022-06-25 23:19:36 -0400

    hopefully fixed pathspec error

---
+++ contracts/src/MediciToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import 'openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
+
+contract MediciToken is ERC20 {
+    constructor() ERC20('Medici', 'MICI') {
+        _mint(msg.sender, 0);
+    }
+
+    function mint(uint256 _lp) public {
+        _mint(msg.sender, _lp);
+    }
+
+    function burn(uint256 _lp) public {
+        _burn(msg.sender, _lp);
+    }
+}
