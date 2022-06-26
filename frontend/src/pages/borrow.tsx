524ac2d11c54c86a4b6424fd2f3919c91b331f29
NME.eth <maximus.holm@gmail.com>
2022-06-25 21:22:34 -0400

    setup sign in with world coin

--- frontend/src/pages/borrow.tsx
+++ frontend/src/pages/borrow.tsx
@@ -1,20 +1,17 @@
 import {
   VStack,
   Text,
+  Box,
 } from '@chakra-ui/react';
 import BorrowerInfo from '../components/BorrowerInfo';
-import { useState } from 'react';
+import worldId from "@worldcoin/id";
+import { useState, useEffect } from 'react';
 
 // borrow limit
 // amount to pay back
 // time limit
-
-/* eslint-disable react/jsx-no-constructed-context-values */
-function Borrow() {
-  const [borrowLimit, setBorrowLimit] = useState(0);
-  const [interest, setInterest] = useState(0);
-  const [dueDate, setDueDate] = useState(0);
-
+const BorrowDashboard = (
+) => {
   return (
     <VStack
       rounded="lg"
@@ -26,20 +23,75 @@ function Borrow() {
       <Text
         fontSize="sm"
       >
-        {`Borrow Limit ${borrowLimit}`}
+        {`Borrow Limit ${0}`}
       </Text>
       <Text
         fontSize="sm"
       >
-        {`Interest ${interest}`}
+        {`Interest ${0}`}
       </Text>
       <Text
         fontSize="sm"
       >
-        {`Due by ${dueDate}`}
+        {`Due by ${0}`}
       </Text>
       <BorrowerInfo />
     </VStack >
+  );
+}
+
+const WorldCoinButton: React.FC = () => {
+  return (
+    <VStack
+      width="100%"
+      spacing={5}
+    >
+      <Text
+        fontWeight="bold"
+      >
+        Sign in with World Coin to borrow
+      </Text>
+      <Box
+        id="world-coin-button"
+      ></Box>
+    </VStack >
+  );
+}
+
+/* eslint-disable react/jsx-no-constructed-context-values */
+function Borrow() {
+  const [borrowLimit, setBorrowLimit] = useState(0);
+  const [interest, setInterest] = useState(0);
+  const [dueDate, setDueDate] = useState();
+
+  useEffect(() => {
+    const setUpWorldId = async () => {
+      if (!worldId.isInitialized()) {
+        worldId.init("world-coin-button", {
+          enable_telemetry: true,
+          action_id: "wid_lshSNnaJfdt6Sohu6YAA",
+          signal: "my-user-id-1",
+          app_name: "My App",
+          signal_description: "Lenders",
+        });
+      }
+      if (!worldId.isEnabled()) {
+        await worldId.enable();
+      }
+    }
+
+    setUpWorldId();
+  });
+
+  return (
+    <>
+      {
+        worldId.isEnabled() ?
+          <BorrowDashboard />
+          :
+          <WorldCoinButton />
+      }
+    </>
   );
 }
 
