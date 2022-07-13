d8fc2b2daa20ac40a8e47d06b089e6d0038c2cd0
aroralanuk <kuarora@ucsd.edu>
2022-07-13 16:33:26 -0400

    sdk deposit test

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,48 +1,45 @@
 import * as React from 'react';
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
-import Lend from '../pages/lend';
+import Deposit from '../pages/deposit';
 import Borrow from '../pages/borrow';
 import Approve from '../pages/approve';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/lend',
-    name: 'Lend',
-    component: Lend,
-  },
-  {
-    path: '/borrow',
-    name: 'Borrow',
-    component: Borrow,
-  },
-  {
-    path: '/approve',
-    name: 'Approve',
-    component: Approve,
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/deposit',
+        name: 'Deposit',
+        component: Deposit,
+    },
+    {
+        path: '/borrow',
+        name: 'Borrow',
+        component: Borrow,
+    },
+    {
+        path: '/approve',
+        name: 'Approve',
+        component: Approve,
+    },
 ];
 
 function Navigation() {
-  return (
-    <Routes>
-      {routes.map((route) => (
-        <Route
-          path={route.path}
-          element={<route.component />}
-          key={route.toString()}
-        />
-      ))}
-    </Routes>
-  );
+    return (
+        <Routes>
+            {routes.map((route) => (
+                <Route
+                    path={route.path}
+                    element={<route.component />}
+                    key={route.toString()}
+                />
+            ))}
+        </Routes>
+    );
 }
 
 export default Navigation;
--- frontend/src/sdk/MediciPool.ts
+++ frontend/src/sdk/MediciPool.ts
@@ -1,5 +1,23 @@
 import { ethers } from 'ethers';
 
-import MediciPoolABI from './abi/MediciPool.json';
+import MediciPoolABI from './abi/MediciPool.abi.json';
+import { MediciPoolAbi } from './types';
 
 const poolInterface = new ethers.utils.Interface(MediciPoolABI.abi);
+
+export default class MediciPool {
+    readonly contract: MediciPoolAbi;
+    contructor(tokenAddress: string) {
+        this.contract = new ethers.Contract(
+            tokenAddress,
+            MediciPoolABI.abi
+        ) as MediciPoolAbi;
+    }
+
+    /**
+     * deposit money to the pool
+     */
+    deposit = async (amount: ethers.BigNumber) => {
+        return this.contract.deposit(amount.toString());
+    };
+}
