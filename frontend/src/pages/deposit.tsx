420176f67ddc4a3572f905ecb568cc1df665b2bc
aroralanuk <kuarora@ucsd.edu>
2022-07-22 02:50:57 -0400

    depsit hook

--- frontend/src/pages/deposit.tsx
+++ frontend/src/pages/deposit.tsx
@@ -7,16 +7,71 @@ import {
     Text,
     VStack,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { createRef } from 'react';
 import { AiOutlineMenu as EthereumCurrency } from 'react-icons/ai';
+import { useContractWrite, useWaitForTransaction } from 'wagmi';
 
 import ApproverInfo from '../components/ApproverInfo';
-import MediciPool from '../sdk/MediciPool';
+import MediciPoolInterface from '../sdk/abi/MediciPool.abi.json';
+
+const contractConfig = {
+    addressOrName: '0x35bfa40f365f8de7707749ba5a483dfef99ed2c9',
+    contractInterface: MediciPoolInterface.abi,
+};
+
+const usdcConfig = {
+    addressOrName: '0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C',
+    contractInterface: MediciPoolInterface.abi,
+};
 
 function Deposit() {
-    const handleDeposit = (event: SyntheticEvent) => {
-        MediciPool.deposit(Number(event.currentTarget.value));
-    };
+    const textInput: React.RefObject<HTMLInputElement> =
+        createRef<HTMLInputElement>();
+    const [depositAmount, setDepositAmount] = React.useState(5);
+
+    const { write: approveUSDC } = useContractWrite(usdcConfig, 'approve', {
+        args: [
+            '0x35bfa40f365f8de7707749ba5a483dfef99ed2c9',
+            100_000_000_000_000,
+        ],
+        overrides: { gasLimit: 1e7 },
+    });
+
+    const {
+        data: depositData,
+        write: handleDeposit,
+        isLoading: isDepositLoading,
+        isSuccess: isDepositSuccess,
+    } = useContractWrite(contractConfig, 'deposit', {
+        args: [],
+        overrides: { gasLimit: 1e7 },
+    });
+
+    // const { depositConfig } = usePrepareContractWrite({
+    //     addressOrName: '0x35bfa40f365f8de7707749ba5a483dfef99ed2c9',
+    //     contractInterface: MediciPoolInterface.abi,
+    //     functionName: 'deposit',
+    //     args: [depositAmount],
+    // });
+
+    // const {
+    //     data: depositData,
+    //     write: handleDeposit,
+    //     isLoading: isDepositLoading,
+    //     isSuccess: isDepositSuccess,
+    // } = useContractWrite({ ...depositConfig });
+
+    const { isSuccess: txSuccess } = useWaitForTransaction({
+        hash: depositData?.hash,
+    });
+
+    const depositSuccess = txSuccess;
+
+    // const handleDeposit = () => {
+    //     console.log(textInput.current?.value);
+    //     // deposit(textInput.current?.value);
+    //     setDepositAmount(textInput.current?.value);
+    // };
 
     return (
         <VStack
@@ -37,8 +92,15 @@ function Deposit() {
                         pointerEvents="none"
                         children={<EthereumCurrency />}
                     />
-                    <Input placeholder="Amount" variant="outline" />
+                    <Input
+                        placeholder="Amount"
+                        variant="outline"
+                        ref={textInput}
+                    />
                 </InputGroup>
+                <Button onClick={() => approveUSDC()} colorScheme="red">
+                    Approve
+                </Button>
                 <Button onClick={() => handleDeposit()} colorScheme="purple">
                     Deposit
                 </Button>
