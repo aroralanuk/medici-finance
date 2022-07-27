baf89876fe2c2a515560d5a70011fc22a6574b59
aroralanuk <kuarora@ucsd.edu>
2022-07-27 00:04:04 -0400

    worldID

--- frontend/src/pages/borrow.tsx
+++ frontend/src/pages/borrow.tsx
@@ -1,98 +1,60 @@
-import {
-  VStack,
-  Text,
-  Box,
-} from '@chakra-ui/react';
+import { VStack, Text, Box } from '@chakra-ui/react';
 import BorrowerInfo from '../components/BorrowerInfo';
-import worldId from "@worldcoin/id";
+import worldId from '@worldcoin/id';
 import { useState, useEffect } from 'react';
+import React from 'react';
 
 // borrow limit
 // amount to pay back
 // time limit
-const BorrowDashboard = (
-) => {
-  return (
-    <VStack
-      rounded="lg"
-      bg="purple.50"
-      as="section"
-      w="100%"
-      p={5}
-    >
-      <Text
-        fontSize="sm"
-      >
-        {`Borrow Limit ${0}`}
-      </Text>
-      <Text
-        fontSize="sm"
-      >
-        {`Interest ${0}`}
-      </Text>
-      <Text
-        fontSize="sm"
-      >
-        {`Due by ${0}`}
-      </Text>
-      <BorrowerInfo />
-    </VStack >
-  );
-}
+const BorrowDashboard = () => {
+    return (
+        <VStack rounded="lg" bg="purple.50" as="section" w="100%" p={5}>
+            <Text fontSize="sm">{`Borrow Limit ${0}`}</Text>
+            <Text fontSize="sm">{`Interest ${0}`}</Text>
+            <Text fontSize="sm">{`Due by ${0}`}</Text>
+            <BorrowerInfo />
+        </VStack>
+    );
+};
 
 const WorldCoinButton: React.FC = () => {
-  return (
-    <VStack
-      width="100%"
-      spacing={5}
-    >
-      <Text
-        fontWeight="bold"
-      >
-        Sign in with World Coin to borrow
-      </Text>
-      <Box
-        id="world-coin-button"
-      ></Box>
-    </VStack >
-  );
-}
+    return (
+        <VStack width="100%" spacing={5}>
+            <Text fontWeight="bold">Sign in with World Coin to borrow</Text>
+            <Box id="world-coin-button"></Box>
+        </VStack>
+    );
+};
 
-/* eslint-disable react/jsx-no-constructed-context-values */
 function Borrow() {
-  const [borrowLimit, setBorrowLimit] = useState(0);
-  const [interest, setInterest] = useState(0);
-  const [dueDate, setDueDate] = useState();
+    const [borrowLimit, setBorrowLimit] = useState(0);
+    const [interest, setInterest] = useState(0);
+    const [dueDate, setDueDate] = useState();
 
-  useEffect(() => {
-    const setUpWorldId = async () => {
-      if (!worldId.isInitialized()) {
-        worldId.init("world-coin-button", {
-          enable_telemetry: true,
-          action_id: "wid_lshSNnaJfdt6Sohu6YAA",
-          signal: "my-user-id-1",
-          app_name: "My App",
-          signal_description: "Lenders",
-        });
-      }
-      if (!worldId.isEnabled()) {
-        await worldId.enable();
-      }
-    }
+    useEffect(() => {
+        const setUpWorldId = async () => {
+            if (!worldId.isInitialized()) {
+                worldId.init('world-coin-button', {
+                    enable_telemetry: true,
+                    action_id: process.env.REACT_APP_WORLDID_ACTIONID,
+                    signal: process.env.REACT_APP_WORLDID_SIGNAL,
+                    app_name: 'unique_borrowers',
+                    signal_description: 'check if the borrower is unique',
+                });
+            }
 
-    setUpWorldId();
-  });
+            if (!worldId.isEnabled()) {
+                await worldId.enable();
+            }
+        };
 
-  return (
-    <>
-      {
-        worldId.isEnabled() ?
-          <BorrowDashboard />
-          :
-          <WorldCoinButton />
-      }
-    </>
-  );
+        setUpWorldId();
+    });
+
+    return (
+        <>{worldId.isEnabled() ? <BorrowDashboard /> : <WorldCoinButton />}</>
+    );
 }
 
 export default Borrow;
